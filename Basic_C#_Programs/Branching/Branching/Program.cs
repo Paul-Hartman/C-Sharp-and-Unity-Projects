5c634c2fdf89c9fa7b1882be55b46516217dd20c
Paul Hartman <78393505+Paul-Hartman@users.noreply.github.com>
2021-11-13 16:33:22 +0100

    program created

--- Basic_C#_Programs/Branching/Branching/Program.cs
+++ Basic_C#_Programs/Branching/Branching/Program.cs
@@ -52,11 +52,39 @@ namespace Branching
             //}
             //Console.ReadLine();
 
-            Console.WriteLine("What is your favorite number?");
-            int favNum = Convert.ToInt32(Console.ReadLine());
-            string results = favNum == 7 ? "you have a good favorite number" : "you do not have anawesome favorite number";
-            Console.WriteLine(results);
-            Console.ReadLine();
+            //Console.WriteLine("What is your favorite number?");
+            //int favNum = Convert.ToInt32(Console.ReadLine());
+            //string results = favNum == 7 ? "you have a good favorite number" : "you do not have anawesome favorite number";
+            //Console.WriteLine(results);
+            //Console.ReadLine();
+
+            int day = 1;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                
+            }
+            Console.Read();
         }
     }
 }
---
+++ Basic_C#_Programs/doWhileAssignment/doWhileAssignment/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doWhileAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("counting");
+            int number = 1;
+            
+            while(number <=10)
+            {
+                Console.WriteLine(number);
+                number++;
+            }
+            Console.WriteLine("guess my age you have 3 tries");
+            int guess = Convert.ToInt32(Console.ReadLine());
+            int tries = 3;
+            bool age = guess == 23;
+
+            do
+            {
+                switch (guess)
+                {
+                    case int n when (n<23):
+                        Console.WriteLine("Too young. try again");
+                        Console.WriteLine("you have "+ tries +" more guesses");
+                        tries--;
+                        guess = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case int n when (n > 23):
+                        Console.WriteLine("Too old. try again. Try again");
+                        Console.WriteLine("you have " + tries + " more guesses");
+                        tries --;
+                        guess = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 23:
+                        Console.WriteLine("That is correct");
+                        age = true;
+                        break;
+                    default:
+                        Console.WriteLine("you are wrong");
+                        Console.WriteLine("you have " + tries + " more guesses");
+                        tries--;
+                        guess = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+            }
+            while (!age && tries>0);
+
+
+
+            Console.Read();
+        }
+    }
+}
