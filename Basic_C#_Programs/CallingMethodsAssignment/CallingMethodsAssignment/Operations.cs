75e3fc12db8aa725028f438cb8332ebe9f80bbd2
Paul Hartman <78393505+Paul-Hartman@users.noreply.github.com>
2021-11-15 15:44:38 +0100

    Created Program

---
+++ Basic_C#_Programs/CallingMethodsAssignment/CallingMethodsAssignment/Operations.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class Operations
+    {
+        static public int Square(int enteredNumber)
+        {
+            int input = enteredNumber;
+            return input * input;
+        }
+
+        static public int AddSub(int newNum)
+        {
+            int input = newNum;
+            if (newNum > 30)
+            {
+                newNum = newNum - 10;
+            }
+            else
+            {
+                newNum = newNum + 15;
+            }
+            return (newNum);
+        }
+
+        static public int IsOdd(int newNum)
+        {
+            int OddorEven = newNum % 2;
+            return (OddorEven);
+        }
+        
+    }
+}
---
+++ Basic_C#_Programs/CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int enteredNumber = Convert.ToInt32(Console.ReadLine());
+            int newNum = Operations.Square(enteredNumber);
+            newNum = Operations.AddSub(newNum);
+            int OddorEven = Operations.IsOdd(newNum);
+            Console.Write(OddorEven);
+            Console.ReadLine();
+        }
+    }
+}
