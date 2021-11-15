a152ea22598c3e29574c113985fd8549f9265bd8
Paul Hartman <78393505+Paul-Hartman@users.noreply.github.com>
2021-11-15 18:53:29 +0100

    Program created

---
+++ Basic_C#_Programs/MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            math go = new math();
+
+            go.doMath(3, 6);
+            int a = 5;
+            int b = 8;
+            go.doMath(a, b);
+            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodClassAssignment/MethodClassAssignment/math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class math
+    {
+        public void doMath (int a, int b)
+        {
+            int c = a*2;
+            Console.WriteLine(b);
+        }
+    }
+}
