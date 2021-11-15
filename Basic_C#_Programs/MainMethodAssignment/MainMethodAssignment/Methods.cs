3933909ec758161bc87f3b011bac46452d6f243a
Paul Hartman <78393505+Paul-Hartman@users.noreply.github.com>
2021-11-15 18:03:50 +0100

    Program Created

---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Methods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class Methods
+    {
+        public static int Subtract(int input)
+        {
+            int diff1 = input - input;
+            return (diff1);
+        }
+        public static float Subtract(float x, float y)
+        {
+            int diff2 = Convert.ToInt32(x - y);
+            return (diff2);
+        }
+        public static int Subtract(string a, string b, string c)
+        {
+            int A =Convert.ToInt32(a);
+            int B =Convert.ToInt32(b);
+            int C =Convert.ToInt32(c);
+            int diff3 = A-B-C;
+            return (diff3);
+        }
+    }
+
+}
---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int input = Convert.ToInt32(Console.ReadLine());
+            
+            
+            int diff1 = Methods.Subtract(input);
+            
+            Console.WriteLine(diff1);
+            
+            float x = 6.6f;
+            float y = 4.4f;
+            int diff2 = Convert.ToInt32(Methods.Subtract(x, y));
+            Console.WriteLine(diff2);
+
+            string a = "24";
+            string b = "14";
+            string c = "3";
+            int diff3 = Methods.Subtract(a, b, c);
+            Console.WriteLine(diff3);
+            Console.ReadLine();
+        }
+    }
+}
