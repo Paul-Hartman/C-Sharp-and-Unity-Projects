635ab953bb9153b09fddbc1660dd6b1c3f54087e
Paul Hartman <78393505+Paul-Hartman@users.noreply.github.com>
2021-11-15 18:36:40 +0100

    Program created

---
+++ Basic_C#_Programs/MethodAssignment/MethodAssignment/Area.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    public class Area
+    {
+        public int area (int l)
+        {
+            int area = l * l;
+            return area;
+        }
+        public int area(int l, int w)
+        {
+            int area = l * w;
+            return area;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+         public static void Main(string[] args)
+        {
+            Console.Write("Enter a whole number\n");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number(optional)\n");
+            string widthstr = Console.ReadLine();
+
+            Area a = new Area();
+
+
+            int square = a.area(length);
+            Console.WriteLine("the area of your shape is " +square);
+
+            if (widthstr != "")
+                {
+                int width = Convert.ToInt32(widthstr);
+
+                int rect = a.area(length, width);
+                Console.WriteLine("The area of your shape is " + rect);
+                 }
+            else
+            {
+                Console.WriteLine("Your shape is a square");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
