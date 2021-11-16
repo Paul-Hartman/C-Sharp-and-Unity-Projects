09964f5d903c6cf1fbfe9ab62d3a09f5242c1cb4
Paul Hartman <78393505+Paul-Hartman@users.noreply.github.com>
2021-11-16 12:11:34 +0100

    Program Created

---
+++ Basic_C#_Programs/MethodsandObjectsAssignment/MethodsandObjectsAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsandObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Emp1 = new Employee();
+            Emp1.FirstName = "Sample";
+            Emp1.LastName = " Student";
+            Emp1.SayName();
+        }
+    }
+}
