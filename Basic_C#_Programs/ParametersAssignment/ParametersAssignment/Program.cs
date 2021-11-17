f5e1744a846b0ea9759c26590f73b1cfe919215c
Paul Hartman <78393505+Paul-Hartman@users.noreply.github.com>
2021-11-17 14:23:59 +0100

    Program created and generic list property created

---
+++ Basic_C#_Programs/ParametersAssignment/ParametersAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Employee<string> A = new Employee<string>();
+            A.Things = new List<string>() { "Pen", "Pencil", "Marker", "Crayon"};
+            
+
+            Employee<int> B = new Employee<int>();
+            B.Things = new List<int>() { 1, 4, 12, 67 };
+            Console.WriteLine("Employee A's things: ");
+            foreach (string thing in A.Things)
+            {
+                Console.WriteLine(thing);
+                
+            }
+
+            Console.WriteLine("\nEmployee B's things: ");
+            foreach (int thing in B.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.ReadLine();
+        }
+    }
+}
