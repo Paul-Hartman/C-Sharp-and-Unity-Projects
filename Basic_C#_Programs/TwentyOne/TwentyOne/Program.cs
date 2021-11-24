67e5b7f64b81c1de2c60db8ab187fd5f3455cfcd
Paul Hartman <78393505+Paul-Hartman@users.noreply.github.com>
2021-11-24 01:37:27 +0100

    Ptogram Completed

--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -10,13 +10,13 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name");
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
             string playerName = Console.ReadLine();
-            Console.WriteLine("How much money did you bring today");
+            Console.WriteLine("How much money did you bring today?");
             int bank = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Hello,{0}, Would you like to join a game of 21 right now", playerName);
+            Console.WriteLine("Hello,{0}, Would you like to join a game of 21 right now?", playerName);
             string answer = Console.ReadLine().ToLower();
-            if (answer == "yes" || answer == "yeah" || answer == "y"|| answer =="ya") ;
+            if (answer == "yes" || answer == "yeah" || answer == "y"|| answer =="ya") 
             {
                 Player player = new Player(playerName, bank);
                 Game game = new TwentyOneGame();
